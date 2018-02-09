870ada0643e3376e60588407ab36ab232a617e32
ihgazni2 <286264978@qq.com>
2018-02-09 14:28:44 +0800

    remove old build

--- build/lib/navegador5/__init__.py
+++ build/lib/navegador5/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['url','head','body','cookie','jq','js_random','file_toolset','shell_cmd','html_tool','solicitud','content_parser']
+__all__ = ['url','head','body','cookie','jq','js_random','file_toolset','shell_cmd','html_tool','solicitud','content_parser','time_utils']
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name="navegador5",
-      version = "0.22",
+      version = "0.23",
       description="tools for http request",
       author="dapeli",
       url="https://github.com/ihgazni2/navegador5",
