498257828e026ec1be511832c01fb4437e99794d
ihgazni2 <286264978@qq.com>
2018-02-11 23:54:03 +0800

    remove old build

--- navegador5/solicitud.py
+++ navegador5/solicitud.py
@@ -110,7 +110,7 @@ def keepalive_init(base_url,**kwargs):
         req_head_str = head.HEAD_POOL[kwargs['UAnum']]
     else:
         req_head_str = '''User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\r\nAccept-Encoding: gzip, deflate, br'''
-    info_container['req_head'] = nvhead.build_headers_dict_from_str(req_head_str,'\r\n')
+    info_container['req_head'] = head.build_headers_dict_from_str(req_head_str,'\r\n')
     info_container['req_head']['Connection'] = 'keep-alive'
     for key in kwargs:
         info_container['req_head'][key] = kwargs[key]
