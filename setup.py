e5f8459a653ab9368fca26a2762afebef672b75d
ihgazni2 <286264978@qq.com>
2018-03-25 17:28:37 +0800

    remove old build

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
       name="navegador5",
-      version = "0.34",
+      version = "0.35",
       description="tools for http request",
       author="dapeli",
       url="https://github.com/ihgazni2/navegador5",
